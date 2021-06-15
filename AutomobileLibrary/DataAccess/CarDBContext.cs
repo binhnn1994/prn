1249d5e3d6cf465d209356b42351aac0c25e9655
binhnn94 <changeguy94@gmail.com>
2021-06-15 16:30:31 +0700

    1st

---
+++ AutomobileLibrary/DataAccess/CarDBContext.cs
@@ -0,0 +1,83 @@
+﻿using AutomobileLibrary.BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomobileLibrary.DataAccess
+{
+    public class CarDBContext
+    {
+        // Initialize car List
+        private List<Car> CarList = new List<Car>()
+        {
+            new Car(){CarID =1, CarName="CRV",Manufacturer ="Honda", Price = 30_000,ReleaseYear = 2021},
+            new Car(){CarID =2, CarName= "Ford Focus",Manufacturer="Ford",Price=15_000, ReleaseYear = 2020}
+        };
+        // Singleton Pattern
+        private static CarDBContext instance = null;
+        private static readonly object instanceLock = new object();
+        private CarDBContext() { }
+        public static CarDBContext Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if(instance == null)
+                    {
+                        instance = new CarDBContext();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public List<Car> GetCarList => CarList;
+        public Car GetCarByID(int carID)
+        {
+            // Using LINQ to Object
+            Car car = CarList.SingleOrDefault(pro => pro.CarID == carID);
+            return car;
+        }
+
+        public void AddNew (Car car)
+        {
+            Car checkCar = GetCarByID(car.CarID);
+            if(checkCar == null)
+            {
+                CarList.Add(car);
+            }
+            else
+            {
+                throw new Exception("Car is already existed.");
+            }
+        }
+
+        public void Update(Car car)
+        {
+            Car checkCar = GetCarByID(car.CarID);
+            if(checkCar != null)
+            {
+                int index = CarList.IndexOf(checkCar);
+                CarList[index] = car;
+            }
+            else
+            {
+                throw new Exception("Car does not already exist.");
+            }
+        }
+        public void Remove(int carID)
+        {
+            Car car = GetCarByID(carID);
+            if(car != null)
+            {
+                CarList.Remove(car);
+            }
+            else
+            {
+                throw new Exception("Car does not already exist.");
+            }
+        }
+    }
+}
---
+++ AutomobileWinApp/frmCarManagement.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace AutomobileWinApp
+{
+    partial class frmCarManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbCarID = new System.Windows.Forms.Label();
+            this.lbCarName = new System.Windows.Forms.Label();
+            this.lbManufacturer = new System.Windows.Forms.Label();
+            this.lbPrice = new System.Windows.Forms.Label();
+            this.lbReleaseYear = new System.Windows.Forms.Label();
+            this.txtCarID = new System.Windows.Forms.TextBox();
+            this.txtCarName = new System.Windows.Forms.TextBox();
+            this.txtManufacturer = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtReleaseYear = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.dgvCarList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbCarID
+            // 
+            this.lbCarID.AutoSize = true;
+            this.lbCarID.Location = new System.Drawing.Point(44, 35);
+            this.lbCarID.Name = "lbCarID";
+            this.lbCarID.Size = new System.Drawing.Size(36, 15);
+            this.lbCarID.TabIndex = 0;
+            this.lbCarID.Text = "CarID";
+            // 
+            // lbCarName
+            // 
+            this.lbCarName.AutoSize = true;
+            this.lbCarName.Location = new System.Drawing.Point(44, 81);
+            this.lbCarName.Name = "lbCarName";
+            this.lbCarName.Size = new System.Drawing.Size(57, 15);
+            this.lbCarName.TabIndex = 1;
+            this.lbCarName.Text = "CarName";
+            // 
+            // lbManufacturer
+            // 
+            this.lbManufacturer.AutoSize = true;
+            this.lbManufacturer.Location = new System.Drawing.Point(44, 129);
+            this.lbManufacturer.Name = "lbManufacturer";
+            this.lbManufacturer.Size = new System.Drawing.Size(79, 15);
+            this.lbManufacturer.TabIndex = 2;
+            this.lbManufacturer.Text = "Manufacturer";
+            // 
+            // lbPrice
+            // 
+            this.lbPrice.AutoSize = true;
+            this.lbPrice.Location = new System.Drawing.Point(380, 35);
+            this.lbPrice.Name = "lbPrice";
+            this.lbPrice.Size = new System.Drawing.Size(33, 15);
+            this.lbPrice.TabIndex = 3;
+            this.lbPrice.Text = "Price";
+            // 
+            // lbReleaseYear
+            // 
+            this.lbReleaseYear.AutoSize = true;
+            this.lbReleaseYear.Location = new System.Drawing.Point(380, 81);
+            this.lbReleaseYear.Name = "lbReleaseYear";
+            this.lbReleaseYear.Size = new System.Drawing.Size(68, 15);
+            this.lbReleaseYear.TabIndex = 4;
+            this.lbReleaseYear.Text = "ReleaseYear";
+            // 
+            // txtCarID
+            // 
+            this.txtCarID.Location = new System.Drawing.Point(140, 32);
+            this.txtCarID.Name = "txtCarID";
+            this.txtCarID.Size = new System.Drawing.Size(191, 23);
+            this.txtCarID.TabIndex = 5;
+            // 
+            // txtCarName
+            // 
+            this.txtCarName.Location = new System.Drawing.Point(140, 78);
+            this.txtCarName.Name = "txtCarName";
+            this.txtCarName.Size = new System.Drawing.Size(191, 23);
+            this.txtCarName.TabIndex = 6;
+            // 
+            // txtManufacturer
+            // 
+            this.txtManufacturer.Location = new System.Drawing.Point(140, 126);
+            this.txtManufacturer.Name = "txtManufacturer";
+            this.txtManufacturer.Size = new System.Drawing.Size(191, 23);
+            this.txtManufacturer.TabIndex = 7;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(458, 32);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(191, 23);
+            this.txtPrice.TabIndex = 8;
+            // 
+            // txtReleaseYear
+            // 
+            this.txtReleaseYear.Location = new System.Drawing.Point(458, 78);
+            this.txtReleaseYear.Name = "txtReleaseYear";
+            this.txtReleaseYear.Size = new System.Drawing.Size(191, 23);
+            this.txtReleaseYear.TabIndex = 9;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(139, 172);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(100, 29);
+            this.btnLoad.TabIndex = 10;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(299, 172);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(100, 29);
+            this.btnNew.TabIndex = 11;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(458, 172);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 29);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(299, 339);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(100, 29);
+            this.btnClose.TabIndex = 13;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // dgvCarList
+            // 
+            this.dgvCarList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCarList.Location = new System.Drawing.Point(9, 207);
+            this.dgvCarList.Name = "dgvCarList";
+            this.dgvCarList.ReadOnly = true;
+            this.dgvCarList.RowTemplate.Height = 25;
+            this.dgvCarList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCarList.Size = new System.Drawing.Size(667, 126);
+            this.dgvCarList.TabIndex = 14;
+            // 
+            // frmCarManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 372);
+            this.Controls.Add(this.dgvCarList);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtReleaseYear);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtManufacturer);
+            this.Controls.Add(this.txtCarName);
+            this.Controls.Add(this.txtCarID);
+            this.Controls.Add(this.lbReleaseYear);
+            this.Controls.Add(this.lbPrice);
+            this.Controls.Add(this.lbManufacturer);
+            this.Controls.Add(this.lbCarName);
+            this.Controls.Add(this.lbCarID);
+            this.Name = "frmCarManagement";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmCarManagement";
+            this.Load += new System.EventHandler(this.frmCarManagement_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCarList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbCarID;
+        private System.Windows.Forms.Label lbCarName;
+        private System.Windows.Forms.Label lbManufacturer;
+        private System.Windows.Forms.Label lbPrice;
+        private System.Windows.Forms.Label lbReleaseYear;
+        private System.Windows.Forms.TextBox txtCarID;
+        private System.Windows.Forms.TextBox txtCarName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtReleaseYear;
+        private System.Windows.Forms.TextBox txtManufacturer;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.DataGridView dgvCarList;
+    }
+}
